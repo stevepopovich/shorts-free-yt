0a437762bc7f5c9425890a8a1713aa9ce7f061cf
Steven Popovich <stevepopovich8@gmail.com>
2023-08-08 21:42:45 -0500

    Mostly working -full screen UI, proper UI layout

--- androidApp/src/main/java/com/stevepopovich/shortsfreeyt/android/MainActivity.kt
+++ androidApp/src/main/java/com/stevepopovich/shortsfreeyt/android/MainActivity.kt
@@ -1,18 +1,15 @@
 package com.stevepopovich.shortsfreeyt.android
 
 import android.app.PictureInPictureParams
-import android.content.Context
 import android.os.Bundle
-import android.view.View
+import android.view.Window
 import android.webkit.CookieManager
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.stevepopovich.shortsfreeyt.URL
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
@@ -39,65 +36,63 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent {
-            MyApplicationTheme {
-                AndroidView(
-                    modifier = Modifier.fillMaxSize(),
-                    factory = {
-                        webView = MediaWebView(this).apply {
-                            webViewClient = object : WebViewClient() {
-                                override fun onPageFinished(view: WebView?, url: String?) {
-                                    GlobalScope.launch {
-                                        delay(1000)
-                                        runOnUiThread {
-                                            view?.evaluateJavascript(
-                                                "var elements = this.document.getElementsByClassName(\"pivot-shorts\");while(elements.length > 0) elements[0].parentNode.removeChild(elements[0])"
-                                            ) {}
+        window.requestFeature(Window.FEATURE_NO_TITLE)
 
-                                            view?.evaluateJavascript(
-                                                "var elements2 = document.getElementsByTagName(\"ytm-reel-shelf-renderer\");while (elements2[0]) elements2[0].parentNode.removeChild(elements2[0]);"
-                                            ) {}
-                                        }
+        webView = MediaWebView(this).apply {
+            webViewClient = object : WebViewClient() {
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    GlobalScope.launch {
+                        delay(1000)
+                        runOnUiThread {
+                            view?.evaluateJavascript(
+                                "var elements = this.document.getElementsByClassName(\"pivot-shorts\");while(elements.length > 0) elements[0].parentNode.removeChild(elements[0])"
+                            ) {}
 
-                                        while (true) {
-                                            delay(300)
-                                            runOnUiThread {
-                                                view?.evaluateJavascript(
-                                                    "elements = this.document.getElementsByClassName(\"pivot-shorts\");while(elements.length > 0) elements[0].parentNode.removeChild(elements[0])"
-                                                ) {}
-
-                                                view?.evaluateJavascript(
-                                                    "elements2 = document.getElementsByTagName(\"ytm-reel-shelf-renderer\");while (elements2[0]) elements2[0].parentNode.removeChild(elements2[0]);"
-                                                ) {}
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-
-                            // Set User Agent
-                            userAgent = System.getProperty("http.agent")
-                            settings.userAgentString = userAgent + "ShortsFreeYT"
-
-                            settings.javaScriptCanOpenWindowsAutomatically = true
-                            settings.setSupportMultipleWindows(true)
-
-                            // WebView Tweaks
-                            settings.javaScriptEnabled = true
-                            settings.domStorageEnabled = true
-                            settings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
-
-                            loadUrl(URL.YOUTUBE)
+                            view?.evaluateJavascript(
+                                "var elements2 = document.getElementsByTagName(\"ytm-reel-shelf-renderer\");while (elements2[0]) elements2[0].parentNode.removeChild(elements2[0]);"
+                            ) {}
                         }
 
-                        // Enable Cookies
-                        CookieManager.getInstance().setAcceptCookie(true)
-                        CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true)
+                        while (true) {
+                            delay(300)
+                            runOnUiThread {
+                                view?.evaluateJavascript(
+                                    "elements = this.document.getElementsByClassName(\"pivot-shorts\");while(elements.length > 0) elements[0].parentNode.removeChild(elements[0])"
+                                ) {}
 
-                        webView
+                                view?.evaluateJavascript(
+                                    "elements2 = document.getElementsByTagName(\"ytm-reel-shelf-renderer\");while (elements2[0]) elements2[0].parentNode.removeChild(elements2[0]);"
+                                ) {}
+                            }
+                        }
                     }
-                )
+                }
             }
+
+            // Set User Agent
+            userAgent = System.getProperty("http.agent")
+            settings.userAgentString = userAgent + "ShortsFreeYT"
+
+            settings.javaScriptCanOpenWindowsAutomatically = true
+            settings.setSupportMultipleWindows(true)
+
+            // WebView Tweaks
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            settings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
+
+            loadUrl(URL.YOUTUBE)
         }
+
+        // Enable Cookies
+        CookieManager.getInstance().setAcceptCookie(true)
+        CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true)
+
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        controller.hide(WindowInsetsCompat.Type.systemBars())
+        controller.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
+        this.setContentView(webView)
     }
 }
